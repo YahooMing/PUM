98534caa66bcbb07658611e356385e0b904995cf
YahooMing <oskich2002@gmail.com>
2025-01-08 22:38:25 +0100

    Lista 8 - ROOM works
    no samouczek działa, to teraz można lecież przerabiać żeby zrobić prawdziwe zadanko

--- Lista8/app/src/main/java/com/example/lista8/MainActivity.kt
+++ Lista8/app/src/main/java/com/example/lista8/MainActivity.kt
@@ -1,17 +1,49 @@
 package com.example.lista8
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.lista8.ui.theme.Lista8Theme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +51,161 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lista8Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+object DataProvider {
+    private val firstNames = listOf(
+        "Adam", "Ewa", "Jan", "Anna", "Piotr", "Maria", "Tomasz", "Małgorzata", "Krzysztof", "Alicja",
+        "Andrzej", "Joanna", "Michał", "Barbara", "Kamil", "Magdalena", "Robert", "Monika", "Mateusz", "Natalia"
     )
+
+    private val lastNames = listOf(
+        "Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk", "Kamiński", "Lewandowski", "Zieliński", "Szymański",
+        "Woźniak", "Dąbrowski", "Kozłowski", "Jankowski", "Mazur", "Kwiatkowski", "Krawczyk", "Piotrowski", "Grabowski",
+        "Nowakowski", "Pawłowski"
+    )
+
+    val user: User
+        get() = User(id = 0 ,firstNames.random(), lastNames.random())
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lista8Theme {
-        Greeting("Android")
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val firstName: String,
+    val lastName: String
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user_table ORDER BY lastName ASC, firstName ASC")
+    fun getUsers(): Flow<List<User>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Query("DELETE FROM user_table")
+    suspend fun deleteAll()
+}
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var Instance: UserDatabase? = null
+
+        fun getDatabase(context: Context): UserDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, UserDatabase::class.java, "user_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
     }
 }
+
+class UserRepository(private val userDao: UserDao) {
+    fun getUsers() = userDao.getUsers()
+    suspend fun clear() = userDao.deleteAll()
+    suspend fun add(user: User) = userDao.insert(user)
+}
+
+class UserViewModelFactory(val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return UserViewModel(application) as T
+    }
+}
+
+class UserViewModel(application: Application) : ViewModel() {
+
+    private val repository: UserRepository
+    private val _usersState = MutableStateFlow<List<User>>(emptyList())
+    val usersState: StateFlow<List<User>>
+        get() = _usersState
+
+    init {
+        val db = UserDatabase.getDatabase(application)
+        val dao = db.userDao()
+        repository = UserRepository(dao)
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        viewModelScope.launch {
+            repository.getUsers().collect { users ->
+                _usersState.value = users
+            }
+        }
+    }
+
+    fun clearUsers() {
+        viewModelScope.launch {
+            repository.clear()
+        }
+    }
+
+    fun addUser(user: User) {
+        viewModelScope.launch {
+            repository.add(user)
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+
+    val viewModel: UserViewModel = viewModel(
+        LocalViewModelStoreOwner.current!!,
+        "UserViewModel",
+        UserViewModelFactory(LocalContext.current.applicationContext as Application)
+    )
+    val users by viewModel.usersState.collectAsStateWithLifecycle()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth().weight(0.7f)
+        ) {
+            items(users.size) {
+                Text(
+                    text = "${users[it].firstName} ${users[it].lastName}",
+                    fontSize = 32.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(2.dp)
+                )
+            }
+        }
+
+        Button(
+            onClick = { viewModel.addUser(DataProvider.user) },
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+        ) {
+            Text(text = "ADD")
+        }
+
+        Button(
+            onClick = { viewModel.clearUsers() },
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+        ) {
+            Text(text = "CLEAR")
+        }
+    }
+}
+
+
+
